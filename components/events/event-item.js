953f71295639d3fc7be4f011d26fd7c7d86cad8e
Huy Nguyen <nhathuy120796@gmail.com>
2023-09-14 19:53:32 +0700

    add UI for project

--- components/events/event-item.js
+++ components/events/event-item.js
@@ -1,16 +1,52 @@
+import Link from "next/link"
+import classes from '../../styles/event-item.module.css'
+import Button from "../ui/button"
+
 function EventItem({ event }) {
-    const { date, id, image, location, title, description } = event
-    const localeDate = new Date(date).toLocaleDateString('en-US', {
+    const { date, id, image, location, title } = event
+    const humanReadableDate = new Date(date).toLocaleDateString('en-US', {
         year: 'numeric',
         month: 'long',
         day: 'numeric'
-    });
+    })
+    const formattedAddress = location.replace(', ', '\n')
+    const exploreLink = `/events/${id}`
     return (
-        <li>
-            <h1>{title}</h1>
-            <p>{description}</p>
+        <li className={classes.item} >
             <img src={`/${image}`} alt={title} />
-            <time>{localeDate}</time>
+            <div className={classes.content}>
+                <div>
+                    <h2>{title}</h2>
+                    <div className={classes.date}>
+                        <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" class="w-6 h-6">
+                            <path stroke-linecap="round" stroke-linejoin="round" d="M6.75 3v2.25M17.25 3v2.25M3 18.75V7.5a2.25 2.25 0 012.25-2.25h13.5A2.25 2.25 0 0121 7.5v11.25m-18 0A2.25 2.25 0 005.25 21h13.5A2.25 2.25 0 0021 18.75m-18 0v-7.5A2.25 2.25 0 015.25 9h13.5A2.25 2.25 0 0121 11.25v7.5m-9-6h.008v.008H12v-.008zM12 15h.008v.008H12V15zm0 2.25h.008v.008H12v-.008zM9.75 15h.008v.008H9.75V15zm0 2.25h.008v.008H9.75v-.008zM7.5 15h.008v.008H7.5V15zm0 2.25h.008v.008H7.5v-.008zm6.75-4.5h.008v.008h-.008v-.008zm0 2.25h.008v.008h-.008V15zm0 2.25h.008v.008h-.008v-.008zm2.25-4.5h.008v.008H16.5v-.008zm0 2.25h.008v.008H16.5V15z" />
+                        </svg>
+                        <time>{humanReadableDate}</time>
+                    </div>
+                    <div className={classes.address}>
+                        <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" class="w-6 h-6">
+                            <path stroke-linecap="round" stroke-linejoin="round" d="M15 10.5a3 3 0 11-6 0 3 3 0 016 0z" />
+                            <path stroke-linecap="round" stroke-linejoin="round" d="M19.5 10.5c0 7.142-7.5 11.25-7.5 11.25S4.5 17.642 4.5 10.5a7.5 7.5 0 1115 0z" />
+                        </svg>
+
+                        <address>{formattedAddress}</address>
+                    </div>
+                </div>
+                <div className={classes.actions}>
+                    <Button link={exploreLink}>
+                        <span>
+                            Exlore Event
+                        </span>
+                        <span className={classes.icon}>
+                            <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" class="w-6 h-6">
+                                <path stroke-linecap="round" stroke-linejoin="round" d="M13.5 4.5L21 12m0 0l-7.5 7.5M21 12H3" />
+                            </svg>
+                        </span>
+
+
+                    </Button>
+                </div>
+            </div>
         </li>
     )
 }
--- components/events/event-list.js
+++ components/events/event-list.js
@@ -1,8 +1,8 @@
 import EventItem from "./event-item"
-
+import classes from "../../styles/event-list.module.css"
 function EventList({ events }) {
     return (
-        <ul>
+        <ul className={classes.list}>
             {
                 events.map((event, index) => (
                     <EventItem 
