99bdbe0f1233ba833cc3584c0ce818010e2f9757
Huy Nguyen <nhathuy120796@gmail.com>
2023-09-15 23:50:13 +0700

    test2

--- components/layout/main-header.js
+++ components/layout/main-header.js
@@ -10,9 +10,7 @@ function MainHeader() {
             <nav className={classes.navigation}>
                 <ul>
                     <li>
-                        
                         <Link href="/events">Browse All Events</Link>
-
                     </li>
                 </ul>
             </nav>
