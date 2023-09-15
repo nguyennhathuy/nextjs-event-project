5a6a65ddff6eef67d64e897c790ddf43802acf3b
Huy Nguyen <nhathuy120796@gmail.com>
2023-09-15 15:58:28 +0700

    add code

---
+++ components/layout/layout.js
@@ -0,0 +1,16 @@
+import MainHeader from "./main-header";
+
+const { Fragment } = require("react");
+
+function Layout(props) {
+    return (
+        <Fragment>
+            <MainHeader />
+            <main>
+                {props.children}
+            </main>
+        </Fragment>
+    )
+}
+
+export default Layout
